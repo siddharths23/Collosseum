880d1427c4cbacbf9e4edb7b9eafcb1f8b8c5233
SethPavlicek <spavlicek@sfsu.edu>
2023-04-12 07:49:03 -0700

    Crab moves with animations (still janky)

---
+++ My project/Assets/CrabAnimationController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KinematicCharacterController.Walkthrough.BasicMovement
+{
+    public class CrabAnimationController : MonoBehaviour
+    {
+        public MyCharacterController controller;
+        public Animator animator;
+
+        private bool tmp = true;
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Debug.Log(controller.moving);
+            // Debug.Log(controller.moving && tmp);
+            if (controller.moving && tmp) {
+                tmp = false;
+                animator.SetTrigger("Walk_Cycle_1");
+            } 
+            else if (!controller.moving) {
+                tmp = true;
+                animator.SetTrigger("Fight_Idle_1");
+            }
+        }
+    }
+}
+
