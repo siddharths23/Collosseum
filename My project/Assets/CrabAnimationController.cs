880097f82f3194a1ea067cc1ffc7207a41ebddc5
SethPavlicek <spavlicek@sfsu.edu>
2023-04-22 07:55:54 -0700

    animation rework confirmed crab moves in multiplayer

--- My project/Assets/CrabAnimationController.cs
+++ My project/Assets/CrabAnimationController.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class CrabAnimationController : MonoBehaviour
+public class CrabAnimationController : NetworkBehaviour
 {
     public Animator animator;
 
-    private bool tmp = true;
-    private bool animated = false;
+    // private bool tmp = true;
+    // private bool animated = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,23 +18,23 @@ public class CrabAnimationController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!animated) {
-            animated = false;
-            animator.SetTrigger("Fight_Idle_1");
-        }
+        // if (!animated) {
+        //     animated = false;
+        //     animator.SetTrigger("Fight_Idle_1");
+        // }
 
-        // Debug.Log(controller.moving);
-        // Debug.Log(controller.moving && tmp);
-        bool moving = ((Input.GetAxisRaw("Horizontal") != 0) || (Input.GetAxisRaw("Vertical") != 0));
-        if (moving && tmp) {
-            tmp = false;
-            animated = true;
-            animator.SetTrigger("Walk_Cycle_1");
-        } 
+        // // Debug.Log(controller.moving);
+        // // Debug.Log(controller.moving && tmp);
+        // bool moving = ((Input.GetAxisRaw("Horizontal") != 0) || (Input.GetAxisRaw("Vertical") != 0));
+        // if (moving && tmp) {
+        //     tmp = false;
+        //     animated = true;
+        //     animator.SetTrigger("Walk_Cycle_1");
+        // } 
 
         if (Input.GetMouseButtonDown(0))
         {
-            animated = true;
+            // animated = true;
             animator.SetTrigger("Attack_1");
         }
     }
--- My project/Assets/CrabMovementController.cs
+++ My project/Assets/CrabMovementController.cs
@@ -7,11 +7,14 @@ public class CrabMovementController : NetworkBehaviour
 {
     public float speed = 30;
     public Rigidbody rigidbody;
+    public Animator animator;
 
     // Update is called once per frame
     void FixedUpdate()
         {
-            if (isLocalPlayer)
+            if (isLocalPlayer) {
                 rigidbody.velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * speed * Time.fixedDeltaTime;
+                animator.SetFloat("Speed", Mathf.Abs(rigidbody.velocity.magnitude));
+            }
         }
 }
