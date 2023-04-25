4e76d35bbf7f0ffc2b2934d2a1fe9523a0af80b3
SethPavlicek <spavlicek@sfsu.edu>
2023-04-24 19:17:12 -0700

    fixed moving animation on 2 handed sword removed animation events

--- My project/Assets/SkeletonMovementController.cs
+++ My project/Assets/SkeletonMovementController.cs
@@ -19,15 +19,14 @@ public class SkeletonMovementController : NetworkBehaviour
     {
         if (isLocalPlayer) {
             rigidbody.velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * speed * Time.fixedDeltaTime;
-            if(Mathf.Abs(rigidbody.velocity.magnitude) > 0.01f) {
-                animator.SetBool("Moving", true);
-                animator.SetFloat("Velocity X", rigidbody.velocity.x);
-                animator.SetFloat("Velocity Z", rigidbody.velocity.z);
-            } else {
-                animator.SetBool("Moving", false);
-            }
-            // animator.SetFloat("Speed", Mathf.Abs(rigidbody.velocity.magnitude));
-            
+            animator.SetFloat("Speed", Mathf.Abs(rigidbody.velocity.magnitude));
+            // if(Mathf.Abs(rigidbody.velocity.magnitude) > 0.01f) {
+            //     animator.SetBool("Moving", true);
+            //     animator.SetFloat("Velocity X", rigidbody.velocity.x);
+            //     animator.SetFloat("Velocity Z", rigidbody.velocity.z);
+            // } else {
+            //     animator.SetBool("Moving", false);
+            // }           
         }
     }
 }
