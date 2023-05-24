886745a3b3594b6c28ffcf53ad224a490d2728fb
Siddharth Sankar <siddharthsankar23@gmail.com>
2023-05-23 18:34:39 -0700

    main menu done, battle background music added

--- My project/Assets/Mirror/Core/NetworkManagerHUD.cs
+++ My project/Assets/Mirror/Core/NetworkManagerHUD.cs
@@ -1,156 +1,115 @@
-// vis2k: GUILayout instead of spacey += ...; removed Update hotkeys to avoid
-// confusion if someone accidentally presses one.
 using UnityEngine;
+using Mirror;
 
-namespace Mirror
+[DisallowMultipleComponent]
+[AddComponentMenu("Network/Network Manager HUD")]
+[RequireComponent(typeof(NetworkManager))]
+[HelpURL("https://mirror-networking.gitbook.io/docs/components/network-manager-hud")]
+public class NetworkManagerHUD : MonoBehaviour
 {
-    /// <summary>Shows NetworkManager controls in a GUI at runtime.</summary>
-    [DisallowMultipleComponent]
-    [AddComponentMenu("Network/Network Manager HUD")]
-    [RequireComponent(typeof(NetworkManager))]
-    [HelpURL("https://mirror-networking.gitbook.io/docs/components/network-manager-hud")]
-    public class NetworkManagerHUD : MonoBehaviour
+    NetworkManager manager;
+    private bool showStartButtons = true;
+    private bool gameHasStarted = false;
+
+    string gameName = "COLLOSSEUM";
+
+
+    void Awake()
     {
-        NetworkManager manager;
+        manager = GetComponent<NetworkManager>();
+    }
 
-        public int offsetX;
-        public int offsetY;
-
-        void Awake()
+    void OnGUI()
+    {
+        if (!gameHasStarted)
         {
-            manager = GetComponent<NetworkManager>();
-        }
+            GUI.backgroundColor = Color.black;
+            GUI.Box(new Rect(0, 0, Screen.width, Screen.height), GUIContent.none);
 
-        void OnGUI()
-        {
-            GUILayout.BeginArea(new Rect(10 + offsetX, 40 + offsetY, 250, 9999));
-            if (!NetworkClient.isConnected && !NetworkServer.active)
-            {
-                StartButtons();
-            }
-            else
-            {
-                StatusLabels();
-            }
+            GUIStyle labelStyle = new GUIStyle(GUI.skin.label);
+            labelStyle.fontSize = 90;
+            labelStyle.fontStyle = FontStyle.Bold;
+            labelStyle.alignment = TextAnchor.MiddleCenter;
+            labelStyle.normal.textColor = Color.red;
 
-            // client ready
-            if (NetworkClient.isConnected && !NetworkClient.ready)
+            GUI.contentColor = Color.white;
+            GUI.Label(new Rect(0, 50, Screen.width, 100), gameName, labelStyle);
+
+            GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
+            buttonStyle.fontSize = 30;
+            buttonStyle.alignment = TextAnchor.MiddleCenter;
+            buttonStyle.normal.textColor = Color.white;
+            buttonStyle.normal.background = MakeTex(2, 2, Color.white);
+
+            GUIStyle buttonStyleHover = new GUIStyle(buttonStyle);
+            buttonStyleHover.normal.textColor = Color.red;
+
+            int buttonWidth = 300;
+            int buttonHeight = 70;
+            int buttonSpacing = 20;
+
+            GUILayout.BeginArea(new Rect(Screen.width / 2 - buttonWidth / 2, 200, buttonWidth, buttonHeight * 3 + buttonSpacing * 2));
+
+            if (showStartButtons)
             {
-                if (GUILayout.Button("Client Ready"))
+                if (!NetworkClient.active && Application.platform != RuntimePlatform.WebGLPlayer)
                 {
-                    NetworkClient.Ready();
-                    if (NetworkClient.localPlayer == null)
-                    {
-                        NetworkClient.AddPlayer();                        
-                    }
-                }
-            }
-
-            StopButtons();
-
-            GUILayout.EndArea();
-        }
-
-        void StartButtons()
-        {
-            if (!NetworkClient.active)
-            {
-                // Server + Client
-                if (Application.platform != RuntimePlatform.WebGLPlayer)
-                {
-                    if (GUILayout.Button("Host Game"))
+                    if (GUILayout.Button("Host Game", buttonStyle))
                     {
                         Debug.Log("Started Server");
                         manager.StartHost();
+                        showStartButtons = false;
+                        gameHasStarted = true;
+
                     }
                 }
 
-                // Client + IP
+                GUILayout.Space(buttonSpacing);
+
                 GUILayout.BeginHorizontal();
-                if (GUILayout.Button("Join Game"))
+                if (GUILayout.Button("Join Game", buttonStyle))
                 {
                     manager.StartClient();
+                    showStartButtons = false;
+                    gameHasStarted = true;
+
                 }
-                // This updates networkAddress every frame from the TextField
                 manager.networkAddress = GUILayout.TextField(manager.networkAddress);
                 GUILayout.EndHorizontal();
 
-                // Server Only
+                GUILayout.Space(buttonSpacing);
+
                 if (Application.platform == RuntimePlatform.WebGLPlayer)
                 {
-                    // cant be a server in webgl build
                     GUILayout.Box("(  WebGL cannot be server  )");
                 }
                 else
                 {
-                    if (GUILayout.Button("Server Only")) manager.StartServer();
-                }
-            }
-            else
-            {
-                // Connecting
-                GUILayout.Label($"Connecting to {manager.networkAddress}..");
-                if (GUILayout.Button("Cancel Connection Attempt"))
-                {
-                    manager.StopClient();
-                }
-            }
-        }
+                    if (GUILayout.Button("Server Only", buttonStyle))
+                    {
+                        manager.StartServer();
+                        showStartButtons = false;
+                        gameHasStarted = true;
 
-        void StatusLabels()
-        {
-            // host mode
-            // display separately because this always confused people:
-            //   Server: ...
-            //   Client: ...
-            if (NetworkServer.active && NetworkClient.active)
-            {
-                GUILayout.Label($"<b>Host</b>: running via {Transport.active}");
+                    }
+                }
             }
-            // server only
-            else if (NetworkServer.active)
-            {
-                GUILayout.Label($"<b>Server</b>: running via {Transport.active}");
-            }
-            // client only
-            else if (NetworkClient.isConnected)
-            {
-                GUILayout.Label($"<b>Client</b>: connected to {manager.networkAddress} via {Transport.active}");
-            }
-        }
 
-        void StopButtons()
-        {
-            // stop host if host mode
-            if (NetworkServer.active && NetworkClient.isConnected)
-            {
-                GUILayout.BeginHorizontal();
-                if (GUILayout.Button("Stop Host"))
-                {
-                    manager.StopHost();
-                }
-                if (GUILayout.Button("Stop Client"))
-                {
-                    manager.StopClient();
-                }
-                GUILayout.EndHorizontal();
-            }
-            // stop client if client-only
-            else if (NetworkClient.isConnected)
-            {
-                if (GUILayout.Button("Stop Client"))
-                {
-                    manager.StopClient();
-                }
-            }
-            // stop server if server-only
-            else if (NetworkServer.active)
-            {
-                if (GUILayout.Button("Stop Server"))
-                {
-                    manager.StopServer();
-                }
-            }
+            GUILayout.EndArea();
         }
+    }
+
+
+    private Texture2D MakeTex(int width, int height, Color col)
+    {
+        Color[] pix = new Color[width * height];
+        for (int i = 0; i < pix.Length; i++)
+            pix[i] = col;
+
+        Texture2D result = new Texture2D(width, height);
+        result.SetPixels(pix);
+        result.Apply();
+
+        return result;
     }
 }
